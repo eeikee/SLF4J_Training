ade10be082ef8b3259d79ad3c5f5a406722b1540
Eike <konumaeike5618@hotmail.com>
2021-02-06 18:09:56 -0300

    add Main class and logger

---
+++ slf4j-training/src/main/java/co/eeikee/slf4jtraining/Main.java
@@ -0,0 +1,12 @@
+package co.eeikee.slf4jtraining;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+
+	public static void main(String[] args) {
+		final Logger LOGGER = LoggerFactory.getLogger(Main.class); 
+	}
+
+}
---
+++ slf4j-training/src/main/java/co/eeikee/slf4jtraining/Slf4jTrainingApplication.java
@@ -0,0 +1,13 @@
+package co.eeikee.slf4jtraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Slf4jTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Slf4jTrainingApplication.class, args);
+	}
+
+}
